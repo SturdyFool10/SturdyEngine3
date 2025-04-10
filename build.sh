d3b533d3c687c10e25ab80285efa3f3afe13bae7
Sturdyfool10 <sturdy@sturdyfool10.com>
2025-04-10 05:01:54 -0600

    Update build script to clean build directory and generate compile_commands.json for improved development workflow

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ..
+#!/bin/bash
+set -e
+
+echo "Cleaning up build directory..."
+rm -rf build
+mkdir build && cd build
+
+echo "Generating build files with compile_commands.json..."
+cmake -G "Unix Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make
+
+[ -f compile_commands.json ] && cp compile_commands.json ..
